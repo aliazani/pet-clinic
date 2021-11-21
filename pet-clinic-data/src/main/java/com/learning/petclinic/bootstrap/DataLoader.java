14d7ba0711a7220ba14635ea10d7e5f80e4cdc48
Mohammadali Azani <aliazn2013@yahoo.com>
2021-11-21 16:36:49 +0330

    Add data initializer

---
+++ pet-clinic-data/src/main/java/com/learning/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,50 @@
+package com.learning.petclinic.bootstrap;
+
+import com.learning.petclinic.model.Owner;
+import com.learning.petclinic.model.Vet;
+import com.learning.petclinic.service.OwnerService;
+import com.learning.petclinic.service.VetService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("John");
+        owner1.setLastName("Steven");
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Markus");
+        owner2.setLastName("Sophia");
+        ownerService.save(owner2);
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Robert");
+        vet1.setLastName("Jobs");
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Mary");
+        vet2.setLastName("Curious");
+        vetService.save(vet2);
+
+        log.info("Data loaded successfully ...");
+    }
+}
