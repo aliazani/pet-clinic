ffec9f62325c78104457f1c5d201a3140f41d39e
Mohammadali Azani <aliazn2013@yahoo.com>
2021-11-25 18:29:13 +0330

    Add error handling to index controller.

--- pet-clinic-web/src/main/java/com/learning/petclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/learning/petclinic/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.learning.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,10 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "welcome";
+    }
+
+    @GetMapping("/oups")
+    public String oups(Model model) {
+        return "error";
     }
 }
