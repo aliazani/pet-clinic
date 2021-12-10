b959ed8c0a13346107e79d6b16b13360bb028b56
Mohammadali Azani <aliazn2013@yahoo.com>
2021-12-10 20:53:54 +0330

    Add cleaner mappings.

---
+++ pet-clinic-web/src/main/java/com/learning/petclinic/mapper/OwnerMapperBetter.java
@@ -0,0 +1,50 @@
+package com.learning.petclinic.mapper;
+
+import com.learning.petclinic.dto.OwnerDto;
+import com.learning.petclinic.model.Owner;
+import org.mapstruct.*;
+
+import java.util.Set;
+
+@Mapper(componentModel = "spring", uses = {PetMapperBetter.class})
+public interface OwnerMapperBetter {
+    // Single - with petDTO(no link to owner)
+    @Named("NoOwner")
+    @Mapping(target = "pets", qualifiedByName = "NoOwner")
+    OwnerDto toDTO(Owner owner);
+
+    // Single - without petDTO
+    @Named("NoPet")
+    @Mapping(target = "pets", ignore = true)
+    OwnerDto toDTONoPet(Owner owner);
+
+    // Set - with petDTO(no link to owner)
+    @Named("NoOwner")
+    @IterableMapping(qualifiedByName = "NoOwner")
+    Set<OwnerDto> toDTOSet(Set<Owner> owner);
+
+    // Set - without petDTO
+    @Named("NoPet")
+    @IterableMapping(qualifiedByName = "NoPet")
+    Set<OwnerDto> toDTOSetNoPet(Set<Owner> owners);
+
+    // Single - with pet(no link to owner)
+    @Named("NoOwner")
+    @Mapping(target = "pets", qualifiedByName = "NoOwner")
+    Owner toEntity(OwnerDto ownerDto);
+
+    // Single - without pet
+    @Named("NoPet")
+    @Mapping(target = "pets", ignore = true)
+    Owner toEntityNoPet(OwnerDto ownerDto);
+
+    // Set - with pet(no link to owner)
+    @Named("NoOwner")
+    @IterableMapping(qualifiedByName = "NoOwner")
+    Set<Owner> toEntitySet(Set<OwnerDto> ownerDtos);
+
+    // Set - without pet
+    @Named("NoPet")
+    @IterableMapping(qualifiedByName = "NoPet")
+    Set<Owner> toEntitySetNoPet(Set<OwnerDto> ownerDtos);
+}
---
+++ pet-clinic-web/src/main/java/com/learning/petclinic/mapper/PetMapperBetter.java
@@ -0,0 +1,80 @@
+package com.learning.petclinic.mapper;
+
+import com.learning.petclinic.dto.PetDto;
+import com.learning.petclinic.model.Pet;
+import org.mapstruct.*;
+
+import java.util.Set;
+
+@Mapper(componentModel = "spring", uses = {PetTypeMapperBetter.class, VisitMapperBetter.class})
+public interface PetMapperBetter {
+    // Single - with visitDTO(no link to petDTO) - with OwnerDTO(no link to petDTO)
+    @Named("NoPet")
+    @Mapping(target = "visits", qualifiedByName = "NoPet")
+//    @Mapping(target = "owner", qualifiedByName = "NoPet")
+    @Mapping(target = "owner.pets", ignore = true)
+    PetDto toDTO(Pet pet);
+
+    // Single - without visitDTO - with OwnerDTO(no link to petDTO)
+    @Named("NoVisit")
+    @Mapping(target = "visits", ignore = true)
+//    @Mapping(target = "owner", qualifiedByName = "NoPet")
+    @Mapping(target = "owner.pets", ignore = true)
+    PetDto toDTONoVisit(Pet pet);
+
+    // Single - with visitDTO(no link to petDTO) - without OwnerDTO
+    @Named("NoOwner")
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "visits", qualifiedByName = "NoPet")
+    PetDto toDTONoOwner(Pet pet);
+
+    // Set - with visitDTO(no link to petDTO) - with OwnerDTO (no link to petDTO)
+    @Named("NoPet")
+    @IterableMapping(qualifiedByName = "NoPet")
+    Set<PetDto> toDTOSet(Set<Pet> pets);
+
+    // Set - without visitDTO - with OwnerDTO (no link to petDTO)
+    @Named("NoVisit")
+    @IterableMapping(qualifiedByName = "NoVisit")
+    Set<PetDto> toDTOSetNoVisit(Set<Pet> pets);
+
+    // Single - with visitDTO(no link to petDTO) - withoutOwner DTO
+    @Named("NoOwner")
+    @IterableMapping(qualifiedByName = "NoOwner")
+    Set<PetDto> toDTOSetNoOwner(Set<Pet> pets);
+
+    // Single - with visit(no link to pet) - with Owner(no link to pet)
+    @Named("NoPet")
+    @Mapping(target = "visits", qualifiedByName = "NoPet")
+    @Mapping(target = "owner.pets", ignore = true)
+//    @Mapping(target = "owner", qualifiedByName = "NoPet")
+    Pet toEntity(PetDto petDto);
+
+    // Single - without visit - with Owner(no link to pet)
+    @Named("NoVisit")
+    @Mapping(target = "visits", ignore = true)
+    @Mapping(target = "owner.pets", ignore = true)
+//    @Mapping(target = "owner", qualifiedByName = "NoPet")
+    Pet toEntityNoVisit(PetDto petDto);
+
+    // Single - with visit(no link to pet) - without Owner
+    @Named("NoOwner")
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "visits", qualifiedByName = "NoPet")
+    Pet toEntityNoOwner(PetDto petDto);
+
+    // Set - with visit(no link to pet) - with Owner(no link to pet)
+    @Named("NoPet")
+    @IterableMapping(qualifiedByName = "NoPet")
+    Set<Pet> toEntitySet(Set<PetDto> petDtos);
+
+    // Set - without visit - with Owner(no link to pet)
+    @Named("NoVisit")
+    @IterableMapping(qualifiedByName = "NoVisit")
+    Set<Pet> toEntitySetNoVisit(Set<PetDto> petDtos);
+
+    // Set - with visit(no link to pet) - without Owner
+    @Named("NoOwner")
+    @IterableMapping(qualifiedByName = "NoOwner")
+    Set<Pet> toEntitySetNoOwner(Set<PetDto> petDtos);
+}
