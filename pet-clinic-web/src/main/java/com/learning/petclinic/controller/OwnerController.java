571d4f52eda3aae4e32c1e9bd0e3b00d3cb73601
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:38 +0330

    Add owner details page.

--- pet-clinic-web/src/main/java/com/learning/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/learning/petclinic/controller/OwnerController.java
@@ -4,6 +4,7 @@ import com.learning.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,5 +26,11 @@ public class OwnerController {
     @GetMapping("/find")
     public String findOwners(Model model) {
         return "owners/findOwners";
+    }
+
+    @GetMapping("/{ownerId}")
+    public String showOwner(@PathVariable("ownerId") Long id, Model model) {
+        model.addAttribute("owner", ownerService.findById(id));
+        return "owners/ownerDetails";
     }
 }
