d2e7dfee4a40d7dc96f98cf5359b5f394be67757
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:37 +0330

    Add speciality for vets.

--- pet-clinic-data/src/main/java/com/learning/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package com.learning.petclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
+
+public class Vet extends Person {
+    Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
