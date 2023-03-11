c503e312f666910eaf88b7c540df6360c09fed4d
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:38 +0330

    Add mappers to convert dto to model obejct and vice versa.

---
+++ pet-clinic-web/src/main/java/com/learning/petclinic/mapper/PetMapper.java
@@ -0,0 +1,17 @@
+package com.learning.petclinic.mapper;
+
+import com.learning.petclinic.dto.PetDto;
+import com.learning.petclinic.model.Pet;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(uses = {PetTypeMapper.class})
+public interface PetMapper {
+    @Mapping(target = "visits", ignore = true)
+    Pet petDtoToPet(PetDto petDto);
+
+    @Mapping(target = "visits", ignore = true)
+    PetDto petToPetDto(Pet pet);
+}
