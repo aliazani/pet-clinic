07b59141cccb4b2eb92822689aa29f647da3ecdf
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:39 +0330

    Fix broken test.

--- pet-clinic-data/src/main/java/com/learning/petclinic/bootstrap/JpaDataLoader.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/bootstrap/JpaDataLoader.java
@@ -14,26 +14,26 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 //@Component
-//@Slf4j
-//@Profile("jpa")
+@Slf4j
+@Profile("jpa")
 public class JpaDataLoader implements CommandLineRunner {
-//    private final OwnerService ownerService;
-//    private final VetService vetService;
-//    private final PetTypeService petTypeService;
-//    private final PetService petService;
-//    private final SpecialityService specialityService;
-//    private final VisitService visitService;
-//
-//    public JpaDataLoader(OwnerService ownerService, VetService vetService,
-//                         PetTypeService petTypeService, PetService petService,
-//                         SpecialityService specialityService, VisitService visitService) {
-//        this.ownerService = ownerService;
-//        this.vetService = vetService;
-//        this.petTypeService = petTypeService;
-//        this.petService = petService;
-//        this.specialityService = specialityService;
-//        this.visitService = visitService;
-//    }
+    private final OwnerService ownerService;
+    private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+    private final SpecialityService specialityService;
+    private final VisitService visitService;
+
+    public JpaDataLoader(OwnerService ownerService, VetService vetService,
+                         PetTypeService petTypeService, PetService petService,
+                         SpecialityService specialityService, VisitService visitService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+        this.specialityService = specialityService;
+        this.visitService = visitService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -41,125 +41,124 @@ public class JpaDataLoader implements CommandLineRunner {
     }
 
     private void loadData() {
-        // PetTypes
-//        PetType dog = PetType.builder()
-//                .name("Dog")
-//                .build();
-//        petTypeService.save(dog);
-//
-//        PetType cat = PetType.builder()
-//                .name("Cat")
-//                .build();
-//        petTypeService.save(cat);
+        PetType dog = PetType.builder()
+                .name("Dog")
+                .build();
+        petTypeService.save(dog);
 
-//        Owner owner1 = Owner.builder()
-//                .firstName("John")
-//                .lastName("Steven")
-//                .address("123 Jay street")
-//                .city("Miami")
-//                .telephone("123456")
-//                .build();
-//        ownerService.save(owner1);
+        PetType cat = PetType.builder()
+                .name("Cat")
+                .build();
+        petTypeService.save(cat);
 
-//        Pet owner1Pet = Pet.builder()
-//                .name("Georgy")
-//                .owner(owner1)
-//                .petType(dog)
-//                .birthDate(LocalDate.now())
-//                .build();
-//        petService.save(owner1Pet);
+        Owner owner1 = Owner.builder()
+                .firstName("John")
+                .lastName("Steven")
+                .address("123 Jay street")
+                .city("Miami")
+                .telephone("123456")
+                .build();
+        ownerService.save(owner1);
 
-//        Visit owner1PetVisit1 = Visit.builder()
-//                .date(LocalDate.now())
-//                .description("John's dog first visit.")
-//                .pet(owner1Pet)
-//                .build();
-//        visitService.save(owner1PetVisit1);
+        Pet owner1Pet = Pet.builder()
+                .name("Georgy")
+                .owner(owner1)
+                .petType(dog)
+                .birthDate(LocalDate.now())
+                .build();
+        petService.save(owner1Pet);
 
-//        Owner owner2 = Owner.builder()
-//                .firstName("Markus")
-//                .lastName("Sophia")
-//                .address("123 Roy street")
-//                .city("Florida")
-//                .telephone("98765")
-//                .build();
-//        ownerService.save(owner2);
+        Visit owner1PetVisit1 = Visit.builder()
+                .date(LocalDate.now())
+                .description("John's dog first visit.")
+                .pet(owner1Pet)
+                .build();
+        visitService.save(owner1PetVisit1);
 
-//        Pet owner2Pet = Pet.builder()
-//                .name("Fiona")
-//                .petType(cat)
-//                .owner(owner2)
-//                .birthDate(LocalDate.now())
-//                .build();
-//        petService.save(owner2Pet);
+        Owner owner2 = Owner.builder()
+                .firstName("Markus")
+                .lastName("Sophia")
+                .address("123 Roy street")
+                .city("Florida")
+                .telephone("98765")
+                .build();
+        ownerService.save(owner2);
 
-//        Visit owner2PetVisit1 = Visit.builder()
-//                .date(LocalDate.now())
-//                .description("first visit to check if the cat is healthy or not.")
-//                .pet(owner2Pet)
-//                .build();
-//        visitService.save(owner2PetVisit1);
+        Pet owner2Pet = Pet.builder()
+                .name("Fiona")
+                .petType(cat)
+                .owner(owner2)
+                .birthDate(LocalDate.now())
+                .build();
+        petService.save(owner2Pet);
 
-//        Visit owner2PetVisit2 = Visit.builder()
-//                .date(LocalDate.now())
-//                .description("second visit to the broken legs.")
-//                .pet(owner2Pet)
-//                .build();
-//        visitService.save(owner2PetVisit2);
+        Visit owner2PetVisit1 = Visit.builder()
+                .date(LocalDate.now())
+                .description("first visit to check if the cat is healthy or not.")
+                .pet(owner2Pet)
+                .build();
+        visitService.save(owner2PetVisit1);
 
-//        Owner owner3 = Owner.builder()
-//                .firstName("Jeff")
-//                .lastName("Sophia")
-//                .address("Love street")
-//                .city("Chicago")
-//                .telephone("828312")
-//                .build();
-//        ownerService.save(owner3);
+        Visit owner2PetVisit2 = Visit.builder()
+                .date(LocalDate.now())
+                .description("second visit to the broken legs.")
+                .pet(owner2Pet)
+                .build();
+        visitService.save(owner2PetVisit2);
 
-//        Pet owner3Pet = Pet.builder()
-//                .name("Jeffy")
-//                .petType(dog)
-//                .owner(owner3)
-//                .birthDate(LocalDate.now())
-//                .build();
-//        petService.save(owner3Pet);
+        Owner owner3 = Owner.builder()
+                .firstName("Jeff")
+                .lastName("Sophia")
+                .address("Love street")
+                .city("Chicago")
+                .telephone("828312")
+                .build();
+        ownerService.save(owner3);
 
-//        Visit owner3PetVisit1 = Visit.builder()
-//                .date(LocalDate.now())
-//                .description("first visit to check if the dog is healthy or not.")
-//                .pet(owner3Pet)
-//                .build();
-//        visitService.save(owner3PetVisit1);
+        Pet owner3Pet = Pet.builder()
+                .name("Jeffy")
+                .petType(dog)
+                .owner(owner3)
+                .birthDate(LocalDate.now())
+                .build();
+        petService.save(owner3Pet);
 
-//        Speciality surgery = Speciality.builder()
-//                .name("Surgery")
-//                .build();
-//        specialityService.save(surgery);
+        Visit owner3PetVisit1 = Visit.builder()
+                .date(LocalDate.now())
+                .description("first visit to check if the dog is healthy or not.")
+                .pet(owner3Pet)
+                .build();
+        visitService.save(owner3PetVisit1);
 
-//        Speciality radiology = Speciality.builder()
-//                .name("Radiology")
-//                .build();
-//        specialityService.save(radiology);
+        Speciality surgery = Speciality.builder()
+                .name("Surgery")
+                .build();
+        specialityService.save(surgery);
 
-//        Speciality dentistry = Speciality.builder()
-//                .name("Dentistry")
-//                .build();
-//        specialityService.save(dentistry);
+        Speciality radiology = Speciality.builder()
+                .name("Radiology")
+                .build();
+        specialityService.save(radiology);
 
-//        Vet vet1 = Vet.builder()
-//                .firstName("Robert")
-//                .lastName("Jobs")
-//                .specialities(Stream.of(radiology, dentistry).collect(Collectors.toCollection(HashSet::new)))
-//                .build();
-//        vetService.save(vet1);
+        Speciality dentistry = Speciality.builder()
+                .name("Dentistry")
+                .build();
+        specialityService.save(dentistry);
 
-//        Vet vet2 = Vet.builder()
-//                .firstName("Mary")
-//                .lastName("Curious")
-//                .specialities(Stream.of(surgery, radiology).collect(Collectors.toCollection(HashSet::new)))
-//                .build();
-//        vetService.save(vet2);
+        Vet vet1 = Vet.builder()
+                .firstName("Robert")
+                .lastName("Jobs")
+                .specialities(Stream.of(radiology, dentistry).collect(Collectors.toCollection(HashSet::new)))
+                .build();
+        vetService.save(vet1);
 
-//        log.info("Data loaded successfully ...");
+        Vet vet2 = Vet.builder()
+                .firstName("Mary")
+                .lastName("Curious")
+                .specialities(Stream.of(surgery, radiology).collect(Collectors.toCollection(HashSet::new)))
+                .build();
+        vetService.save(vet2);
+
+        log.info("Data loaded successfully ...");
     }
 }
