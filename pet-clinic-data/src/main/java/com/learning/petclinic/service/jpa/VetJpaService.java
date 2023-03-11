7ca61431c8d3ab63a4a9ef10d56523e679034506
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:38 +0330

    Add profiles for jpa and map implementations.

--- pet-clinic-data/src/main/java/com/learning/petclinic/service/jpa/VetJpaService.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/service/jpa/VetJpaService.java
@@ -3,11 +3,13 @@ package com.learning.petclinic.service.jpa;
 import com.learning.petclinic.model.Vet;
 import com.learning.petclinic.repository.VetRepository;
 import com.learning.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile("jpa")
 public class VetJpaService implements VetService {
     private final VetRepository vetRepository;
 
--- pet-clinic-data/src/main/java/com/learning/petclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/service/map/VetMapService.java
@@ -3,11 +3,13 @@ package com.learning.petclinic.service.map;
 import com.learning.petclinic.model.Vet;
 import com.learning.petclinic.service.SpecialityService;
 import com.learning.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile("map")
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
     private final SpecialityService specialityService;
 
