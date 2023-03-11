23b158e2bc4280babb2671e61b6400ade372942a
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:38 +0330

    Add json endpoint for getVets.

--- pet-clinic-web/src/main/java/com/learning/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/learning/petclinic/controller/VetController.java
@@ -1,10 +1,14 @@
 package com.learning.petclinic.controller;
 
+import com.learning.petclinic.model.Vet;
 import com.learning.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -19,5 +23,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/vetList";
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
