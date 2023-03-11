0a27b115f2a2bf8db1078448e054b1ab0853b739
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:38 +0330

    Enhance save method for VisitMapSerivce.

--- pet-clinic-data/src/main/java/com/learning/petclinic/service/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/service/map/VisitMapService.java
@@ -28,6 +28,9 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
 
     @Override
     public Visit save(Visit item) {
+        if (item.getPet() == null || item.getPet().getPetType() == null || item.getPet().getOwner() == null)
+            throw new RuntimeException("Invalid Visit.");
+
         return super.save(item);
     }
 
