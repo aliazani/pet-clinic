23bee4623f20f7787a5fc54781d7bd451d6fec62
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:38 +0330

    Add JPA service implementations.

--- pet-clinic-data/src/main/java/com/learning/petclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/repository/OwnerRepository.java
@@ -3,5 +3,8 @@ package com.learning.petclinic.repository;
 import com.learning.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Optional<Owner> findByLastName(String lastName);
 }
