70721d0c761dd50c2cd8c1258aea09ec7de7156c
Mohammadali Azani <ali_azani@yahoo.com>
2023-03-11 22:03:37 +0330

    Add pet and contact info into dataloader.

--- pet-clinic-data/src/main/java/com/learning/petclinic/service/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/service/map/OwnerMapService.java
@@ -2,12 +2,22 @@ package com.learning.petclinic.service.map;
 
 import com.learning.petclinic.model.Owner;
 import com.learning.petclinic.service.OwnerService;
+import com.learning.petclinic.service.PetService;
+import com.learning.petclinic.service.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public OwnerMapService(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,7 +35,17 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner item) {
-        return super.save(item);
+        if (item.getPets() != null) {
+            item.getPets().forEach(pet -> {
+                if (pet.getId() == null)
+                    pet.setId(petService.save(pet).getId());
+                if (pet.getPetType() != null && pet.getPetType().getId() == null)
+                    pet.setPetType(petTypeService.save(pet.getPetType()));
+            });
+            return super.save(item);
+        }
+
+        return null;
     }
 
     @Override
